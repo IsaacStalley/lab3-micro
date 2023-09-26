beace8bf0111d1d7c857118354b1d832f391baa5
Isaac Stalley <isaacstalley@gmail.com>
2023-09-26 15:31:29 -0600

    Added code for warning LEDS

--- src/voltmeter/voltmeter.ino
+++ src/voltmeter/voltmeter.ino
@@ -13,6 +13,10 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(8, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
   // PCD8544-compatible displays may have a different resolution...
   lcd.begin(84, 48);
 
@@ -27,7 +31,6 @@ float get_max_voltage(int port){
   float max_voltage = abs(reverse_TF(port));
   float voltage = abs(reverse_TF(port));
   int counter = 0;
-
   while(1){
     if (voltage > max_voltage){
       while (1){
@@ -37,6 +40,9 @@ float get_max_voltage(int port){
           return max_voltage;
         }
       }
+    }
+    else if (voltage == max_voltage){
+      return max_voltage;
     }
     else {
       max_voltage = voltage;
@@ -56,12 +62,29 @@ void loop() {
   float voltageA3 = reverse_TF(A3);
 
   if (switch_DC) {
-    lcd.setCursor(0, 0);
-    lcd.print("DC Voltages");
-    lcd.setCursor(0, 1);
-    lcd.print("------------");
-  
-  }
+    if (voltageA0 > 20 || voltageA0 < -20){
+      digitalWrite(13, HIGH);
+    }
+    else {digitalWrite(13, LOW);}
+    if (voltageA1 > 20 || voltageA1 < -20){
+      digitalWrite(12, HIGH);
+    }
+    else {digitalWrite(12, LOW);}
+    if (voltageA2 > 20 || voltageA2 < -20){
+      digitalWrite(11, HIGH);
+    }
+    else {digitalWrite(11, LOW);}
+    if (voltageA3 > 20 || voltageA3 < -20){
+      digitalWrite(10, HIGH);
+    }
+    else {digitalWrite(10, LOW);}
+
+      lcd.setCursor(0, 0);
+      lcd.print("DC Voltages");
+      lcd.setCursor(0, 1);
+      lcd.print("-----------");
+    
+    }
 
   else {
     float max_voltage = get_max_voltage(A0);
@@ -89,10 +112,27 @@ void loop() {
     }
     voltageA3 = global_max_voltageA3 / sqrt(2);
 
+    if (global_max_voltageA0 > 20 || global_max_voltageA0 < -20){
+      digitalWrite(13, HIGH);
+    }
+    else {digitalWrite(13, LOW);}
+    if (global_max_voltageA1 > 20 || global_max_voltageA1 < -20){
+      digitalWrite(12, HIGH);
+    }
+    else {digitalWrite(12, LOW);}
+    if (global_max_voltageA2 > 20 || global_max_voltageA2 < -20){
+      digitalWrite(11, HIGH);
+    }
+    else {digitalWrite(11, LOW);}
+    if (global_max_voltageA3 > 20 || global_max_voltageA3 < -20){
+      digitalWrite(10, HIGH);
+    }
+    else {digitalWrite(10, LOW);}
+
     lcd.setCursor(0, 0);
     lcd.print("AC Voltages");
     lcd.setCursor(0, 1);
-    lcd.print("------------");
+    lcd.print("-----------");
   }
 
   // Write a piece of text on the first line...
